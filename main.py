60168d636ccb0458fa1f2b1010d705936351727c
Chuanqi Sun <chuanqi.sun.15@gmail.com>
2026-08-21 01:36:31 -0400

    switch to main model

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ else:
 
 providers = [("MIGraphXExecutionProvider", migraphx_options)]
 
-session = ort.InferenceSession("models/yolo26n-seg.onnx", providers=providers)
+session = ort.InferenceSession("models/yolo26s-seg.onnx", providers=providers)
 input_name = session.get_inputs()[0].name
 INPUT_SIZE = 640
 
